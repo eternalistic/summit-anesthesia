9daaf617a685b87bc65e164ffc99a4de5e57bfc6
Jeremy Caldwell <jeremy@eternalistic.net>
2024-07-09 08:02:19 -0700

    Cleanup and fixed HTML format for site code

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,28 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
+
+  // Navigation plugin.
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // Shortcode for year.
   eleventyConfig.addShortcode('year', () => {
     return `${new Date().getFullYear()}`;
   });
+
+  // Passthrough for assets.
   [
     'src/css',
     'src/images/',
     'src/js/',
     {'src/images/favicon': '/'}
-  ].forEach(path =>
-    eleventyConfig.addPassthroughCopy(path)
-  );
+  ].forEach(path => {
+    eleventyConfig.addPassthroughCopy(path);
+  });
+
+  // Template formats and directories.
   return {
+    templateFormats: ["html", "liquid", "njk"],
     dir: {
       input: 'src',
       output: 'dist',
